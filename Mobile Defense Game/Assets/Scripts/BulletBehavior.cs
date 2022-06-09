085edaa81906595d1f271e71b36a1d57766f9a0f
cei0907 <cei0907@naver.com>
2022-06-10 01:19:11 +0900

    총알 발사 관련 소스코드 개선

--- Mobile Defense Game/Assets/Scripts/BulletBehavior.cs
+++ Mobile Defense Game/Assets/Scripts/BulletBehavior.cs
@@ -3,23 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour {
+    //총알의 행동을 정의하는 script
 
-    public float speed = 10.0f;
+    public BulletStat bulletStat { get; set; }
     public GameObject character;
 
-    private int damage;
-
-    public void setDamage(int input)
+    public BulletBehavior()
     {
-        damage = input;
+        bulletStat = new BulletStat(0, 0);
+        //BulletBehavior가 초기화가 될 때 BulletStat도 초기화 되도록
     }
+
+
 
 	void Start () {
         Destroy(gameObject, 3.0f);
 	}
 	
 	void Update () {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        transform.Translate(Vector2.right * bulletStat.speed * Time.deltaTime);
 	}
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -27,7 +29,8 @@ public class BulletBehavior : MonoBehaviour {
         if(other.gameObject.tag == "Monster")
         {
             Destroy(gameObject);
-            other.GetComponent<MonsterStat>().attacked(damage);
+            other.GetComponent<MonsterStat>().attacked(bulletStat.damage);
+            //other의 컴포넌트<MonsterStat>을 가져와서 그 안에 attacked함수를 실행시켜라 이런 의미인듯
         }   
     }
 }
---
+++ Mobile Defense Game/Assets/Scripts/BulletStat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//VO : Value Object :데이터의 속성 명시
+public class BulletStat 
+{
+    public float speed { get; set; }
+    public int damage { get; set; }
+
+    public BulletStat(float speed, int damage)
+    {
+        this.speed = speed;
+        this.damage = damage;
+    }
+    // Start is called before the first frame update
+}
